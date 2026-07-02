d30b1224e45f66831cbc61a7a231ba9d4cb8e888
Vitalis Gachigua <matherivitalis@gmail.com>
2026-07-02 13:17:56 +0300

    Implement add food functionality

--- script.js
+++ script.js
@@ -19,7 +19,36 @@ foodForm.addEventListener("submit", function (event) {
         alert("Please fill in all fields.");
         return;
     }
-
     console.log(foodName);
     console.log(calories);
+
+     const food = {
+        id: Date.now(),
+        name: foodName,
+        calories: Number(calories)
+    };
+
+    foods.push(food);
+    const foodHTML = `
+        <div class="flex justify-between items-center bg-slate-100 p-3 rounded-lg mb-2">
+            <div>
+                <p class="font-semibold">${food.name}</p>
+                <p class="text-sm text-gray-600">${food.calories} Calories</p>
+            </div>
+
+            <button class="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded-lg">
+                Remove
+            </button>
+        </div>
+    `;
+
+    foodList.innerHTML += foodHTML;
+    
+    console.log(foods);
+
+    foodNameInput.value = "";
+    caloriesInput.value = "";
 });
+
+    
+
